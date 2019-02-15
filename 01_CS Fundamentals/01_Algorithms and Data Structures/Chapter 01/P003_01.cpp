1318b7e7b95f7287665beca5448ef66bfdf0a225
Susung Park <sspark010@gmail.com>
2019-02-15 17:25:20 +0900

    checkout @ P.72

---
+++ 01_CS Fundamentals/01_Algorithms and Data Structures/Chapter 01/P003_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(void)
+{
+    int x, y;
+    std::cout << "Please enter two numbers >> ";
+    std::cin >> x >> y;
+    int sum = x + y;
+    std::cout << "Their sum is " << sum << std::endl;
+
+    return EXIT_SUCCESS;
+}
