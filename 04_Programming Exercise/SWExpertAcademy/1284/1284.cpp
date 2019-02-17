5fabc19129b67e54a0245e833081d305e778315e
Susung Park <sspark010@gmail.com>
2019-02-17 14:13:17 +0900

    solved swexpertacademy #1284

---
+++ 04_Programming Exercise/SWExpertAcademy/1284/1284.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int min(int a, int b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+int tapWaterFare(int fareA, int baseB, int thresB, int fareB, int liters)
+{
+    int aCompany = 0;
+    int bCompany = 0;
+
+    aCompany = fareA * liters;
+    bCompany = baseB + fareB * max(liters - thresB, 0);
+
+    return min(aCompany, bCompany);
+}
+
+int main(void)
+{
+    int tcCnt;
+    int fareA, int baseB, int thresB, int fareB, int liters;
+    int minFare;
+    std::cin >> tcCnt;
+
+    for (int tc = 1; tc <= tcCnt; tc++)
+    {
+        std::cin >> fareA >> baseB >> thresB >> fareB >> liters;
+        minFare = tapWaterFare(fareA, baseB, thresB, fareB, liters);
+        std::cout << "#" << tc << " " << minFare << std::endl;
+    }
+
+    return 0;
+}
