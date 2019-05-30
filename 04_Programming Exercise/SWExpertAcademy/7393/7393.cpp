b5f98f144100a5585f6da04f508a0da5d269ede4
Susung Park <susung@susung-macbook.local>
2019-05-30 12:56:56 +0900

    solving swexpertacademy #7393

--- 04_Programming Exercise/SWExpertAcademy/7393/7393.cpp
+++ 04_Programming Exercise/SWExpertAcademy/7393/7393.cpp
@@ -1,90 +1,51 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
 
 int main(void)
 {
+    // Replace stdin with file.
     freopen("input.txt", "r", stdin);
-    std::vector< std::vector< std::vector<long> > > numbers(101);
+
+    // Define parameters.
+    int tcCnt;
+    std::cin >> tcCnt;
+    int maxLen = 10, tcLen;
+    int tcMatchCnt;
+
+    // Create and initialize table for DP.
+    std::vector< std::vector<long> > numbers(101);
     for (int i = 0; i < numbers.size(); i++)
     {
         numbers[i].resize(10);
         for (int j = 0; j < numbers[i].size(); j++)
-        {
-            numbers[i][j].resize(10);
-            for (int k = 0; k < numbers[i][j].size(); k++)
-                numbers[i][j][k] = 0;
-        }
+            numbers[i][j] = 0;
     }
+    numbers[10][0] = numbers[10][9] = 1;
 
-    int tcCnt, tcNum, caseCnt;
-    scanf("%d", &tcCnt);
-    int len = 0, lenMaxExpanded = 9;
-
+    // Evaluate test cases.
     for (int tc = 1; tc <= tcCnt; tc++)
     {
-        scanf("%d", &tcNum);
-        ++lenMaxExpanded;
-        for (len = lenMaxExpanded; len <= tcNum; len++)
+        std::cin >> tcLen;
+        for (int len = ++maxLen; len <= tcLen; len++)
         {
-            printf("%d \n", len);
+            printf("len = %d \n", len);
             for (int firstDigit = 0; firstDigit <= 9; firstDigit++)
             {
-                if (len == 10)
-                {
-                    if (firstDigit == 0)
-                        numbers[len][firstDigit][9] = 1;
-                    else if (firstDigit == 9)
-                        numbers[len][firstDigit][0] = 1;
-                }
+                if (firstDigit == 0)
+                    numbers[len][0] = numbers[len - 1][1];
+                else if (firstDigit == 9)
+                    numbers[len][9] = numbers[len - 1][8];
                 else
-                {
-                    if (firstDigit == 0)
-                    {
-                        for (int lastDigit = 0; lastDigit <= 9; lastDigit++)
-                        {
-                            if (lastDigit == 0)
-                                numbers[len][1][lastDigit] = numbers[len][1][1];
-                            else if (lastDigit == 9)
-                                numbers[len][1][lastDigit] = numbers[len][1][8];
-                            else
-                                numbers[len][1][lastDigit] = (numbers[len][1][1] + numbers[len][1][8]) % 1000000000;
-                        }
-                    }
-                    else if (firstDigit == 9)
-                    {
-                        for (int lastDigit = 0; lastDigit <= 9; lastDigit++)
-                        {
-                            if (lastDigit == 0)
-                                numbers[len][8][lastDigit] = numbers[len][8][1];
-                            else if (lastDigit == 9)
-                                numbers[len][8][lastDigit] = numbers[len][8][8];
-                            else
-                                numbers[len][8][lastDigit] = (numbers[len][8][1] + numbers[len][8][8]) % 1000000000;
-                        }
-                    }
-                    else
-                    {
-                        for (int lastDigit = 0; lastDigit <= 9; lastDigit++)
-                        {
-                            if (lastDigit == 0)
-                                numbers[len][firstDigit][lastDigit] += numbers[len][firstDigit][1];
-                            else if (lastDigit == 9)
-                                numbers[len][firstDigit][lastDigit] += numbers[len][firstDigit][8];
-                            else
-                                numbers[len][firstDigit][lastDigit] += (numbers[len][firstDigit][1] + numbers[len][firstDigit][8]) % 1000000000;
-                        }
-                    }
-                }
+                    numbers[len][firstDigit] = numbers[len - 1][firstDigit + 1] +
+                                               numbers[len - 1][firstDigit - 1];
             }
         }
-        --len;
-        lenMaxExpanded = len;
+        maxLen = tcLen;
 
-        caseCnt = 0;
+        tcMatchCnt = 0;
         for (int i = 1; i <= 9; i++)
-            for (int j = 0; j <=9; j++)
-            caseCnt += (numbers[tcNum][i][j] % 1000000000);
+            tcMatchCnt += numbers[tcLen][i];
         
-        printf("#%d %ld \n", tc, caseCnt % 1000000000);
+        printf("#%d %d \n", tc, tcMatchCnt);
     }
 }
--- 04_Programming Exercise/SWExpertAcademy/7393/notes.md
+++ 04_Programming Exercise/SWExpertAcademy/7393/notes.md
@@ -1,4 +1,3 @@
 2D Array of numbers:
 axis 0: length
 axis 1: first digit
-axis 2: last digit
