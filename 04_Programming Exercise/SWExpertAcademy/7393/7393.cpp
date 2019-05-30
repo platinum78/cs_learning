dbcf91622406b34f6cc0ed09c44eb428e221254f
Susung Park <susung@susung-macbook.local>
2019-05-30 13:46:21 +0900

    solved swexpertacademy #7393

--- 04_Programming Exercise/SWExpertAcademy/7393/7393.cpp
+++ 04_Programming Exercise/SWExpertAcademy/7393/7393.cpp
@@ -1,51 +1,41 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
+#include <memory.h>
+#define MOD 1000000000
+
+int dpTable[101][10][1 << 10];
+
+int findNum(int len, int digit, int inclusion)
+{
+    if (digit < 0 || digit > 9)
+        return 0;
+    if (len == 1)
+        return (1 << digit | inclusion) == 1023;
+    int &num = dpTable[len][digit][inclusion];
+    if (num != -1)
+        return num;
+
+    inclusion |= (1 << digit);
+    num = (findNum(len - 1, digit - 1, inclusion) +
+           findNum(len - 1, digit + 1, inclusion)) % MOD;
+    return num;
+}
 
 int main(void)
 {
-    // Replace stdin with file.
     freopen("input.txt", "r", stdin);
 
-    // Define parameters.
-    int tcCnt;
-    std::cin >> tcCnt;
-    int maxLen = 10, tcLen;
-    int tcMatchCnt;
+    int tcCnt, tcLen, tcMatchCnt;
+    scanf("%d", &tcCnt);
+    memset(dpTable, -1, sizeof(dpTable));
 
-    // Create and initialize table for DP.
-    std::vector< std::vector<long> > numbers(101);
-    for (int i = 0; i < numbers.size(); i++)
-    {
-        numbers[i].resize(10);
-        for (int j = 0; j < numbers[i].size(); j++)
-            numbers[i][j] = 0;
-    }
-    numbers[10][0] = numbers[10][9] = 1;
-
-    // Evaluate test cases.
     for (int tc = 1; tc <= tcCnt; tc++)
     {
-        std::cin >> tcLen;
-        for (int len = ++maxLen; len <= tcLen; len++)
-        {
-            printf("len = %d \n", len);
-            for (int firstDigit = 0; firstDigit <= 9; firstDigit++)
-            {
-                if (firstDigit == 0)
-                    numbers[len][0] = numbers[len - 1][1];
-                else if (firstDigit == 9)
-                    numbers[len][9] = numbers[len - 1][8];
-                else
-                    numbers[len][firstDigit] = numbers[len - 1][firstDigit + 1] +
-                                               numbers[len - 1][firstDigit - 1];
-            }
-        }
-        maxLen = tcLen;
-
+        scanf("%d", &tcLen);
         tcMatchCnt = 0;
-        for (int i = 1; i <= 9; i++)
-            tcMatchCnt += numbers[tcLen][i];
-        
+        for (int digit = 1; digit <= 9; digit++)
+            tcMatchCnt = (tcMatchCnt + findNum(tcLen, digit, 0)) % MOD;
         printf("#%d %d \n", tc, tcMatchCnt);
     }
+
+    return 0;
 }
