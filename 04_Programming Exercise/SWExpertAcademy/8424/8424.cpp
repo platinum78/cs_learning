c02f1e60e11fbd99ce253ff75e3a5938d4b8a8fe
Susung Park <susung@susung-macbook.local>
2019-09-10 11:45:54 +0900

    solved swexpertacademy #8424

--- 04_Programming Exercise/SWExpertAcademy/8424/8424.cpp
+++ 04_Programming Exercise/SWExpertAcademy/8424/8424.cpp
@@ -1,54 +1,68 @@
 #include <cstdio>
 
+int order[1001];
+bool visited[1001];
+int nodeCnt;
+int loopPos, loopLen;
+bool **cxnMat;
+
+bool dfs(int node, int prev, int depth)
+{
+    order[node] = depth;
+    bool found = false;
+
+    for (int i = 1; i <= nodeCnt; i++)
+    {
+        if (cxnMat[node][i])
+        {
+            if (!visited[i])
+            {
+                visited[i] = true;
+                found = dfs(i, node, depth + 1);
+                visited[i] = false;
+                if (found)
+                    return true;
+            }
+            else if (i != prev)
+            {
+                loopPos = i;
+                loopLen = depth - order[i] + 1;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main(void)
 {
     freopen("input.txt", "r", stdin);
 
-    int **g = new int*[1001];
+    cxnMat = new bool*[1001];
     for (int i = 0; i < 1001; i++)
-        g[i] = new int[4];
-    int tcCnt, edgeCnt;
-    int n1, n2, tailLen = 0;
-    int thisNode, prevNode;
+        cxnMat[i] = new bool[1001];
+
+    int tcCnt, edgeCnt, x, y;
     scanf("%d", &tcCnt);
     
     for (int tc = 1; tc <= tcCnt; tc++)
     {
         scanf("%d", &edgeCnt);
-        for (int i = 1; i <= edgeCnt; i++)
-            g[i][0] = 0;
+        nodeCnt = edgeCnt;
+        for (int i = 0; i <= edgeCnt; i++)
+        {
+            visited[i] = false;
+            for (int j = 0; j <= edgeCnt; j++)
+                cxnMat[i][j] = false;
+        }
+
+        for (int i = 0; i < edgeCnt; i++)
+        {
+            scanf("%d %d", &x, &y);
+            cxnMat[x][y] = cxnMat[y][x] = true;
+        }
         
-        for (int i = 1; i <= edgeCnt; i++)
-        {
-            scanf("%d %d", &n1, &n2);
-            g[n1][0]++;
-            g[n2][0]++;
-            g[n1][g[n1][0]] = n2;
-            g[n2][g[n2][0]] = n1;
-        }
-
-        for (int i = 1; i <= edgeCnt; i++)
-        {
-            if (g[i][0] == 1)
-            {
-                prevNode = 0; thisNode = i;
-                while (g[thisNode][0] <= 2)
-                {
-                    if (g[thisNode][1] != prevNode)
-                    {
-                        prevNode = thisNode;
-                        thisNode = g[thisNode][1];
-                    }
-                    else
-                    {
-                        prevNode = thisNode;
-                        thisNode = g[thisNode][2];
-                    }
-                    tailLen++;
-                }
-            }
-        }
-
-        printf("#%d %d \n", tc, edgeCnt - tailLen);
+        dfs(1, 0, 0);
+        printf("#%d %d \n", tc, loopLen);
     }
 }
