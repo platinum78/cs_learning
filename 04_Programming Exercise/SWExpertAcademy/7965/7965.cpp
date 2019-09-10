abf2dd8e14a3182666f2ddd8fceb86eb7d8ac336
Susung Park <susung@susung-macbook.local>
2019-09-10 10:04:01 +0900

    solved swexpertacademy #7965

--- 04_Programming Exercise/SWExpertAcademy/7965/7965.cpp
+++ 04_Programming Exercise/SWExpertAcademy/7965/7965.cpp
@@ -2,44 +2,46 @@
 #include <cmath>
 #define DIV 1000000007
 
-long modPower(long x)
+int *refTable;
+
+unsigned long modPower(unsigned long x, unsigned long pow)
 {
-    long coef = 1, num = x, mul = 0, rem = 1, rem_total = 1;
-    long x_ = 0;
-    while (num > 0)
-    {
-        mul = num % 2;
-        num /= 2;
-        rem = x % DIV;
+    if (pow == 0)
+        return 1;
 
-        if (mul)
-        {
-            for (int i = 0; i < coef - 2; i++)
-            {
-                rem = (rem * rem) % DIV;
-            }
-            x_ += pow(2, coef - 1);
-        }
-        rem_total = (rem_total * rem) % DIV;
-        coef++;
-    }
+    unsigned long x_, pow_half;
+    pow_half = pow / 2;
+    x_ = modPower(x, pow_half);
+    x_ = (x_ * x_) % DIV;
+    if (pow % 2)
+        x_ = (x_ * x) % DIV;
 
-    printf("%lu \n", x_);
-
-    return rem_total;
+    return x_;
 }
+
 
 int main(void)
 {
     freopen("input.txt", "r", stdin);
 
     int tcCnt;
-    long maxNum, num;
+    unsigned long maxNum = 0, num, sum;
+    refTable = new int[1000001];
     scanf("%d", &tcCnt);
 
     for (int tc = 1; tc <= tcCnt; tc++)
     {
-        scanf("%lu", &maxNum);
-        printf("#%d %lu \n", tc, modPower(maxNum));
+        scanf("%lu", &num);
+        while (maxNum < num)
+        {
+            maxNum++;
+            refTable[maxNum] = modPower(maxNum, maxNum);
+        }
+        
+        sum = 0;
+        for (int i = 1; i <= num; i++)
+            sum = (sum + refTable[i]) % DIV;
+        
+        printf("#%d %lu \n", tc, sum);
     }
 }
